06b73e1b69ed7c8ecc651e554f4b5988f45b9831
frakod <frakod@pm.me>
2023-03-21 19:56:26 +0000

    changed to GET only api because of IDF bugs

--- main/api.hpp
+++ main/api.hpp
@@ -25,7 +25,14 @@ bool check_admin(httpd_req_t* req)
     
     // last 32 bits of ipv6 should gives us ipv4
     auto ip4 = addr.sin6_addr.un.u32_addr[3];
-    auto mac = online_clients.at(ip4);
+    
+    mac_address mac;
+    
+    try {mac = online_clients.at(ip4);} catch(...)
+    {
+        log::warn("Time Traveler Detected !!\n");
+        return false;
+    }
     
     if (admin_list.contains(mac))
     {
@@ -44,7 +51,7 @@ bool check_admin(httpd_req_t* req)
         return false;
     }
 }
-
+/*
 bool get_content(httpd_req_t* req, std::string& dst_buf, size_t max_len)
 {
     dst_buf.resize(max_len);
@@ -70,10 +77,10 @@ bool get_content(httpd_req_t* req, std::string& dst_buf, size_t max_len)
         return false;
     }
 }
-
+*/
 esp_err_t ping_fn(httpd_req_t* req)
 {
-    return httpd_resp_send(req, "UwU", 3);
+    return httpd_resp_sendstr(req, "UwU");
 }
 
 esp_err_t access_logs_fn(httpd_req_t* req)
@@ -141,14 +148,36 @@ esp_err_t add_user_fn(httpd_req_t* req)
     
     std::string content;
     
-    if (!get_content(req, content, 128))
-        return ESP_FAIL;
+    mac_address mac;
+    std::string name, query;
+    auto query_len = httpd_req_get_url_query_len(req);
     
-    auto name = content.substr(18);
-    auto  mac = str2mac(content.substr(0, 17));
+    if (query_len)
+    {
+        query.resize(query_len);
+        if (httpd_req_get_url_query_str(req, query.data(), query.size()+1) != ESP_OK)
+            goto query_error;
+        
+        char buf[13]{}; // eg. 1a2b3c3d2e1f
+        if (httpd_query_key_value(query.c_str(), "mac", buf, sizeof(buf)) != ESP_OK)
+            goto query_error;
+        
+        if (std::sscanf(buf, "%012llx", &mac) != 1)
+            goto query_error;
+        
+        name.resize(48);
+        if (httpd_query_key_value(query.c_str(), "name", name.data(), name.size()) != ESP_OK)
+            goto query_error;
+        
+        for (char& ch : name) if (ch == '~') ch = ' '; // all spaces must be replaced with ~ by client
+    }
+    else goto query_error;
+    
     user_manager.add_user(mac, name);
-    
     return httpd_resp_send(req, "OK", 2);
+    
+query_error:
+    return httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "invalid query");
 }
 
 esp_err_t remove_user_fn(httpd_req_t* req)
@@ -156,12 +185,24 @@ esp_err_t remove_user_fn(httpd_req_t* req)
     if (!check_admin(req))
         return httpd_resp_send_err(req, HTTPD_403_FORBIDDEN, "access denied");
     
-    std::string content;
+    mac_address mac;
+    std::string query;
+    auto query_len = httpd_req_get_url_query_len(req);
     
-    if (!get_content(req, content, 17))
-        return ESP_FAIL;
-    
-    auto mac = str2mac(content);
+    if (query_len)
+    {
+        query.resize(query_len);
+        if (httpd_req_get_url_query_str(req, query.data(), query.size()+1) != ESP_OK)
+            goto query_error;
+        
+        char buf[13]{}; // eg. 1a2b3c3d2e1f
+        if (httpd_query_key_value(query.c_str(), "mac", buf, sizeof(buf)) != ESP_OK)
+            goto query_error;
+        
+        if (std::sscanf(buf, "%012llx", &mac) != 1)
+            goto query_error;
+    }
+    else goto query_error;
     
     if (user_manager.check_user(mac))
     {
@@ -169,6 +210,9 @@ esp_err_t remove_user_fn(httpd_req_t* req)
         return httpd_resp_send(req, "OK", 2);
     }
     else return httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "User Not Found");
+    
+query_error:
+    return httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "invalid query");
 }
 
 esp_err_t check_user_fn(httpd_req_t* req)
@@ -176,56 +220,71 @@ esp_err_t check_user_fn(httpd_req_t* req)
     if (!check_admin(req))
         return httpd_resp_send_err(req, HTTPD_403_FORBIDDEN, "access denied");
     
-    std::string content;
+    mac_address mac;
+    std::string query;
+    auto query_len = httpd_req_get_url_query_len(req);
     
-    if (!get_content(req, content, 17))
-        return ESP_FAIL;
-    
-    auto mac = str2mac(content);
+    if (query_len)
+    {
+        query.resize(query_len);
+        if (httpd_req_get_url_query_str(req, query.data(), query.size()+1) != ESP_OK)
+            goto query_error;
+        
+        char buf[13]{}; // eg. 1a2b3c3d2e1f
+        if (httpd_query_key_value(query.c_str(), "mac", buf, sizeof(buf)) != ESP_OK)
+            goto query_error;
+        
+        if (std::sscanf(buf, "%012llx", &mac) != 1)
+            goto query_error;
+    }
+    else goto query_error;
     
     if (user_manager.check_user(mac))
         return httpd_resp_sendstr(req, user_manager.get_username(mac).c_str());
     else
         return httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "User Not Found");
+    
+query_error:
+    return httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "invalid query");
 }
 
 /////////////////////////////////////////////
 
 httpd_uri_t ping
 {
-    .uri      = "/ping",
-    .method   = HTTP_GET,
-    .handler  = ping_fn
+    .uri     = "/ping",
+    .method  = HTTP_GET,
+    .handler = ping_fn
 };
 httpd_uri_t access_logs
 {
-    .uri      = "/access_logs",
-    .method   = HTTP_GET,
-    .handler  = access_logs_fn
+    .uri     = "/access_logs",
+    .method  = HTTP_GET,
+    .handler = access_logs_fn
 };
 httpd_uri_t user_list
 {
-    .uri      = "/user_list",
-    .method   = HTTP_GET,
-    .handler  = user_list_fn
+    .uri     = "/user_list",
+    .method  = HTTP_GET,
+    .handler = user_list_fn
 };
 httpd_uri_t add_user
 {
-    .uri      = "/add_user",
-    .method   = HTTP_POST,
-    .handler  = add_user_fn
+    .uri     = "/add_user",
+    .method  = HTTP_GET,
+    .handler = add_user_fn
 };
 httpd_uri_t remove_user
 {
-    .uri      = "/remove_user",
-    .method   = HTTP_POST,
-    .handler  = remove_user_fn
+    .uri     = "/remove_user",
+    .method  = HTTP_GET,
+    .handler = remove_user_fn
 };
 httpd_uri_t check_user
 {
-    .uri      = "/check_user",
-    .method   = HTTP_POST,
-    .handler  = check_user_fn
+    .uri     = "/check_user",
+    .method  = HTTP_GET,
+    .handler = check_user_fn
 };
 
 }
